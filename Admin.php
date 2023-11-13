5bfdfa18b1e517510fa6e72444e7e679e6467ff2
8mileverse <Ayodelu4@gmail.com>
2023-11-13 09:01:18 +0100

    trial

---
+++ Admin.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Admin Page</title>
+
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    
+<div class="container">
+    <div class="content">
+        <h3>Hi, Admin 🖐🖐</h3>
+        
+        <h1><i>Welcome <span></span></i></h1>
+
+        <p>This is an Admin Page</p>
+
+        <a href="Login_Form.php" class="btn">Log In</a>
+
+        <a href="Register.php" class="btn">Register</a>
+
+        <a href="Forget.php" class="btn">Forget</a>
+
+
+
+
+
+    </div>
+</div>
+
+</body>
+</html>
